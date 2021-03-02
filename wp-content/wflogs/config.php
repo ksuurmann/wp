abda34e633d101b356c89582c49d75764e62a4f4
kaisasuurmannikt <kaisa.suurmann@khk.ee>
2021-03-02 15:31:50 +0200

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1614174841;s:7:"authKey";s:64:"9gK!biGMIZgZ3YkKKHZYI~aP;WMG%g=L>#,AQ3=h(L0#RX 4JpLXlEX!xZG>n?Ju";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1586;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"9gK!biGMIZgZ3YkKKHZYI~aP;WMG%g=L>#,AQ3=h(L0#RX 4JpLXlEX!xZG>n?Ju";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1586;}
