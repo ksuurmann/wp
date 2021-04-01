71c0efd9ec74950df8074033a28447cbbc46d327
kaisasuurmannikt <kaisa.suurmann@khk.ee>
2021-04-01 22:25:34 +0300

    modified plugin `Very First Plugin` version 1.0

---
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Very First Plugin
+* Plugin URI: https://kaisasuurmann.ikt.khk.ee/wordpress/
+* Description: This is the very first plugin I ever created.
+* Version: 1.0
+* Author: Kaisa Suurmann
+* Author URI: https://kaisasuurmann.ikt.khk.ee/wordpress/
+**/
