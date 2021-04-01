e0083712c4208b3048eeb8d0b92113e12ce16c53
kaisasuurmannikt <kaisa.suurmann@khk.ee>
2021-04-01 22:26:02 +0300

    installed plugin `Very First Plugin` version 1.0

--- wp-content/plugins/very-first-plugin/very-first-plugin.php
+++ wp-content/plugins/very-first-plugin/very-first-plugin.php
@@ -7,3 +7,8 @@
 * Author: Kaisa Suurmann
 * Author URI: https://kaisasuurmann.ikt.khk.ee/wordpress/
 **/
+
+function dh_modify_read_more_link() {
+ return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter( 'the_content_more_link', 'dh_modify_read_more_link' );
